1bfbb7ec78b1d4d1b63ac87bb874610d206051a3
alifthi <alifathi8008@gmail.com>
2023-10-12 15:39:07 -0400

    person_clustering: extract_persons class defined to extracting the persons in the images

---
+++ codes/data_related/extract_persons.py
@@ -0,0 +1,13 @@
+import cv2 
+import numpy as np
+from glob import glob
+
+class extract_persons:
+    def __init__(self) -> None:
+        pass
+    def load_extractor(self):
+        pass
+    def check_extracted(self):
+        pass
+    def extract_persons(self):
+        pass
