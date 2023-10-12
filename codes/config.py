2ad81e338953c5adc5f58b8483f185ef97418ee8
alifthi <alifathi8008@gmail.com>
2023-10-12 12:00:53 -0400

    person_clustering: config file added for saving the variables

---
+++ codes/config.py
@@ -0,0 +1,7 @@
+EXTRACTED_MASKED_PERSON_PATH='../data/masked_players/*'
+EXTRACTED_PERSON_PATH=''
+MAIN_PICTURES_PATH='../data/pics'
+COCO_PB='../models/person_detector/frozen_inference_graph_coco.pb'
+COCO_PBTXT='../models/person_detector/mask_rcnn_inception_v2_coco_2018_01_28.pbtxt'
+COCO_NAMES='../models/person_detector/coco.names'
+MASKES=True
