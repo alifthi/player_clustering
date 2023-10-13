3f34af8d8ec02439e214564cad191629c0a3c67d
alifthi <alifathi8008@gmail.com>
2023-10-13 10:48:49 -0400

    person_clustering: methods of train_model defined

--- codes/train_model.py
+++ codes/train_model.py
@@ -1,3 +1,14 @@
 class train_model:
     def __init__(self) -> None:
         pass
+    def train_model(self):
+        pass
+    def save_model(self):
+        pass
+    def load_model(self):
+        pass
+    def compile_model(self):
+        pass
+    def build_model(self):
+        pass
+    
