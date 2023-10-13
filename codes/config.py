f5e2cf14cdd0043a6c263ea35c0181f2aa345323
alifthi <alifathi8008@gmail.com>
2023-10-13 11:22:51 -0400

    person_clustering: variables related to model added to config file

--- codes/config.py
+++ codes/config.py
@@ -5,3 +5,8 @@ COCO_PB='../models/person_detector/frozen_inference_graph_coco.pb'
 COCO_PBTXT='../models/person_detector/mask_rcnn_inception_v2_coco_2018_01_28.pbtxt'
 COCO_NAMES='../models/person_detector/coco.names'
 MASKED=True
+
+SAVE_MODEL_PATH='../models/'
+INPUT_SHAPE=[64,64,3]
+EMBEDDING_DIM=4
+
