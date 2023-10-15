61c1f37398c59830228bd10349c14bdd8a1e70a4
alifthi <alifathi8008@gmail.com>
2023-10-15 13:52:24 -0400

    person_clustering: extraction file developed

---
+++ codes/extraction.py
@@ -0,0 +1,5 @@
+from data_related.extract_persons import extract_persons
+extract_persons = extract_persons()
+extract_persons.check_extracted()
+extract_persons.load_extractor()
+extract_persons.extract_persons()
